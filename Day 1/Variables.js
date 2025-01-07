55bfd59e49c84ef5479a08bc52b8ce1f0e8a4807
shiva9111 <rshivamanish@gmail.com>
2025-01-07 09:07:19 +0530

    shiva

---
+++ Day 1/Variables.js
@@ -0,0 +1,25 @@
+console.log("start");
+
+
+
+//global Scope
+
+
+var a = 10;
+console.log(a)
+
+
+//Local Scope
+
+let b = 20;
+console.log(b)
+
+
+const c = 30;
+console.log(c)
+
+//block Scope
+
+
+
+console.log("end");
---
+++ Day 2/app.js
@@ -0,0 +1,29 @@
+let str1 = "double quotes";
+console.log(str1 , typeof(str1));
+
+let str2 = "single quotes";
+console.log(str2 , typeof(str2));
+
+let str3 = "template literals";
+console.log(str3 , typeof(str3));
+
+let bool1 = true;
+console.log(bool1, typeof(bool1));
+
+let bool2 = false;
+console.log(bool2, typeof(bool2));
+
+let num1 = 10;
+console.log(num1 , typeof(num1));
+
+let num2 = 10.2345;
+console.log(num3 , typeof(num2));
+let num3 = 10e3
+console.log(num3 , typeof(num3));
+
+
+let a;
+console.log(a , typeof(x));
+
+let x= null;
+console.log(x , typeof(x));
