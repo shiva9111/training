2979301e6f9c77539cf3e954fedd4b4adacb15bc
shiva9111 <rshivamanish@gmail.com>
2025-01-07 15:17:32 +0530

    day3

---
+++ Day 3/conditions.js
@@ -0,0 +1,48 @@
+if(true)
+{
+    console.log("if block is executing");
+}
+
+
+
+let isAdult = 10;
+if(isAdult >= 18){
+    console.log("he/she is major");
+}else{
+    console.log("he/she is minor");
+
+}
+
+isAdult >= 18 ?console.log("Major"):console.log("Minor");
+
+let day = prompt("Enter a Number");
+if(day == 1){
+    console.log("Sunday");
+
+
+}else if(day == 2){
+    console.log("Monday");
+
+
+}else if(day == 3){
+    console.log("Tuesday");
+
+}else if(day == 4){
+    console.log("wed");
+
+}else if(day == 5){
+    console.log("thurs");
+
+}else if(day == 6){
+    console.log("fri");
+
+
+}
+else if(day==7){
+    console.log("saturday");
+}
+
+else{
+    console.log("Error");
+
+}
---
+++ Day 3/operators.js
@@ -0,0 +1,94 @@
+console.log(5+5);
+console.log(100+1000);
+console.log(10+"100"); //concatenation
+console.log(10+20+"HI");
+console.log("HI"+10+50);
+
+//Subtraction
+console.log(10-5);
+console.log("10"-5); //typecasting
+console.log("10"-"2"); //typecasting
+
+
+//Multiplication
+console.log(5*5);
+console.log(5*"5");
+
+//Division
+console.log(10/5);
+console.log("10"/5); //typecasting
+
+//Expo
+console.log(2**5);
+console.log(5**2);
+console.log("5"**"5"); //typecasting
+
+//mod
+console.log(10%2);
+console.log("10"%5); //typecasting
+
+//increment  --preincrement(++before increment)  --postincrement(++after increment)
+let a = 10;
+console.log(a);
+console.log(++a);
+console.log(a++);
+
+
+//Decrement
+let b= 20;
+console.log(--a);
+console.log(a);
+
+
+//Assignment Opearator
+let c = 10;
+console.log(a+5);
+console.log(a+=5); //!a = a+5
+console.log(a-=5);
+console.log(a*=5);
+console.log(a**=5);
+console.log(a%=5);
+
+//Realtional Operator
+//return type of relation operator boolean values
+
+console.log(10==10);
+console.log("10"==10);
+console.log(100==10);
+console.log("HI"=="HI");
+console.log(10===10);//3===
+console.log("10"==10);
+console.log(10!=5);
+console.log(100 !="100");
+console.log(10 !== 10);
+console.log("10" !== 10); //true !== and strictly not equals to
+console.log(10>6);
+console.log(10<6);
+console.log(10>10);//false
+console.log(10>=10);//true
+console.log(10<20);
+console.log(20<=20);
+
+
+//Logical Operators
+//both lhs and rhs if it is true the value printed 
+//or anyone can be true
+console.log(10>5) && (5>10);//And flase
+console.log(10>5) || (10>20);//or
+console.log(!true);
+console.log(!false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Desision Making
+
