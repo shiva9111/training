bb74c4cebe66e0a8a2655839d6f8841a53af10fd
shiva9111 <rshivamanish@gmail.com>
2025-01-08 13:19:11 +0530

    new files added

---
+++ Day 4/looping.js
@@ -0,0 +1,28 @@
+// // for(let i=1; i<=5;i++){
+// //     console.log("JavaScript");
+// // }
+
+// for(let i=1; i<=100;i++){
+//     if( i % 2 == 0){
+//         console.log(i +"is a even number");
+
+//     }
+//     else{
+//         console.log(i + "is an odd number");
+//     }
+// }
+ 
+//while loop
+// let i=1;
+// while(i<= 5){
+//     console.log("Hello Wake and please interact with him");
+//     i++;
+// }
+
+//do while loop
+
+do
+{
+    console.log("Hello JavaScript");
+}while(i<=5);
+
