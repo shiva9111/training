c3c6bd7dcaa97d6bcbff07151b578b3860d73f9f
shiva9111 <rshivamanish@gmail.com>
2025-01-20 11:44:44 +0530

    new add

---
+++ Day 7/arrays.js
@@ -0,0 +1,29 @@
+// let user1= "shiva";
+// let user2= "shiva";
+// let user3= "shiva";
+// let user4= "shiva";
+// let user5= "shiva";
+
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+// console.log(user5);
+
+// let users = ["Sanjay","ramu", "rajiv","samuel", "john"];
+
+// users.map(user, i)=>{
+//     console.log(users,i)
+// }
+// let x= users.map((user,i)=> {
+//     return users;
+// })
+// console.log(x)
+
+let users = ["Shiva","raju", "goku","samurai", "john"];
+
+let x = users.forEach((users, i)=>{
+    return users;
+});
+console.log(x);
